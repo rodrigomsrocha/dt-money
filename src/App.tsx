d26f02c1268c4d77f9f4b2eb24bf577fe66456dd
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-09-18 11:36:22 -0300

    Revert "setting up fonts"
    This reverts commit 72e0966fe9484b7e25921b1cb5ae7ddad9ee7d10.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 import { GlobalStyle } from "./styles/global";
 
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 3rem;
+`;
+
 export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
       <GlobalStyle />
     </div>
   );
