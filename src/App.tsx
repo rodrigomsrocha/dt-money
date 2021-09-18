bf6f9e4283fa5de04db54a9985692d6f80e04181
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-09-18 11:37:51 -0300

    setting up fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import styled from "styled-components";
 import { GlobalStyle } from "./styles/global";
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 3rem;
-`;
 
 export function App() {
   return (
     <div>
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
       <GlobalStyle />
     </div>
   );
