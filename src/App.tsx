eb43f09f317c3468dc555a767aeb094fe57c4245
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-09-18 15:22:31 -0300

    setting up fake api, get route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,25 @@ import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 
 import { GlobalStyle } from "./styles/global";
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transition 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 export function App() {
   return (
